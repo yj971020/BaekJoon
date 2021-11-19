887b5a8399bccdfa7af419d678e0ccca67afdd76
yj971020 <yj971020@naver.com>
2021-11-20 00:44:16 +0900

    11/20

--- yeajin/src/yeajin/Main.java
+++ yeajin/src/yeajin/Main.java
@@ -10,7 +10,7 @@ public class Main {
 		int n=sc.nextInt();
 		int[] arr=new int[n];
 		for(int i=0;i<n;i++) {
-			arr[i]= sc.nextInt();
+			arr[i]=sc.nextInt();
 		}
 		for(int i=0;i<arr.length;i++) {
 			
